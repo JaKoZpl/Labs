f5fdb0d40d64a343eb616ed19b92bb5f4daf87b0
JaKoZpl <rekryt02@gmail.com>
2022-12-27 23:01:32 +0200

    Lab2-routers
    need modify

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 import express from 'express';
+import items from './Data/items.json';
+import _ from 'lodash';
 
+const BASE_ITEMS_URL = '/api/v1/items';
 const PORT = 3000;
 const server = express();
 console.log("Hello, world!");
 const message = 'Hello NodeJs!!!!';
 console.log(message);
-server.listen(PORT, () => {
-    console.log('Server has been started on port ' + PORT)
-})
 
 server.get('/', (req, res) => {
     res.send("Hello my js-route!");
 });
+
+
+
+server.get(BASE_ITEMS_URL, (req, res) =>{
+    res.json(items);
+})
+server.get(BASE_ITEMS_URL+'/:id', (req, res) =>{
+    const id = req.params.id;
+    const item = _.find(items, item => item.id === id);
+    if(item) {
+        res.json(item);
+    }else{
+        res.send('NOT FOUND');
+    }
+})
+
+
