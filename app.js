011b33132047e6cc97f469f5a8063a9efd39f9b3
JaKoZpl <rekryt02@gmail.com>
2022-12-28 15:01:27 +0200

    Lab5-template-EJS

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import items from './Data/items.json';
 import _ from 'lodash';
 import ItemRoute from './routes/ItemRoute';
 import morgan from 'morgan';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
+import path from 'path';
+
 const BASE_ITEMS_URL = '/api/v1/items';
 
 const server = express();
@@ -16,9 +18,13 @@ console.log(message);
 
 server.use(morgan('tiny'));
 server.use(bodyParser.json());
+server.set('views', path.join('views'));
+server.set('view engine', 'ejs');
 
-
-server.use(BASE_ITEMS_URL, ItemRoute)
+server.use(BASE_ITEMS_URL, ItemRoute);
+server.get('/', (req, res) =>{
+    res.render('index', {items});
+});
 
 server.get('/', (req, res) => {
     console.log("Request to root");
